0f204e5d0019e52d69eeb60875f450d78c12a109
Your Name <you@example.com>
2025-05-13 09:57:33 -0700

    update3

--- main.cpp
+++ main.cpp
@@ -36,7 +36,26 @@ class TicketQueue {
                 dequeue();
             }
         }
-        void enqueue(string gpuName, string model, string issue){
-            
+        void enqueue(string gpuName, string model, string issue){// Add a new ticket to the queue
+            TicketNode * newTicket = new TickeNode(gpuName, model, issue); // Create a new ticket node
+            if (tail == nullptr){ // If the queue is empty
+                head = tail = newTicket;
+            } else {// If the queue is not empty
+                tail -> next = newTicket;
+                tail = newTicket;
+            }
+            cout << " Ticket created: " << gpuName << " " << model << " " << issue << endl;
+        }
+
+        void dequeue() {
+            if (head==nullptr){
+                cout << "No tickets in the queue." << endl;
+                return;
+            }
+            TicketNode * temp = head; // Store the current head
+            head = head -> next; // Move the head to the next ticket
+            if (head == nullptr) tail = nullptr;
+            cout << "Ticket resolved: " << temp->gpuName<< " (" <<temp->model << ")\n";
+            delete temp; // Delete the resolved ticket"
         }
 }
